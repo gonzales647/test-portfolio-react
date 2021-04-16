3346035c58d99d781541fd52aaf5f025e5363848
gonzales647 <82166951+gonzales647@users.noreply.github.com>
2021-04-15 22:15:59 -0700

    test portfolio day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import Header from './Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Header />
       <div className="content">
         <h1>My Portfolio test</h1>
       </div>
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+    return (
+        <div classname='nav'>
+            <nav className="navbar navbar-default">
+                <h1>James's Portfolio</h1>
+                <div className="links">
+                    <a href='/'>Home</a>
+                    <a href='/create'>Other Page</a>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Navbar;
